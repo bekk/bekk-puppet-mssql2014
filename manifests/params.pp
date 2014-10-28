27132a94272d493e38528b857ef5bfe1b2ab61f0
PierrickI3 <PierrickI3@users.noreply.github.com>
2014-10-28 17:14:12 +0100

    Updating for basic SQL 2014 settings

--- manifests/params.pp
+++ manifests/params.pp
@@ -8,10 +8,6 @@
 # $features
 # $agtsvcaccount
 # $agtsvcpassword
-# $assvcaccount
-# $assvcpassword
-# $rssvcaccount
-# $rssvcpassword
 # $sqlsvcaccount
 # $sqlsvcpassword
 # $instancedir
@@ -21,15 +17,11 @@
 #
 class mssql::params {
   $instancename   = 'MSSQLSERVER'
-  $features       = 'SQL,AS,RS,IS'
+  $features       = 'SQL,IS'
   $agtsvcaccount  = 'SQLAGTSVC'
-  $agtsvcpassword = 'Sql!@gt#2008demo'
-  $assvcaccount   = 'SQLASSVC'
-  $assvcpassword  = 'Sql!@s#2008demo'
-  $rssvcaccount   = 'SQLRSSVC'
-  $rssvcpassword  = 'Sql!Rs#2008demo'
+  $agtsvcpassword = 'D0gf00d'
   $sqlsvcaccount  = 'SQLSVC'
-  $sqlsvcpassword = 'Sql!#2008demo'
+  $sqlsvcpassword = 'D0gf00d'
   $instancedir    = 'C:\Program Files\Microsoft SQL Server\MSSQL10_50.MSSQLSERVER\MSSQL\Binn\sqlservr.exe'
   $ascollation    = 'Latin1_General_CI_AS'
   $sqlcollation   = 'SQL_Latin1_General_CP1_CI_AS'
