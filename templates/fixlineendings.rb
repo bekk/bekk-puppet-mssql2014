abda44e74d54278378b0576c79edfe51445a8ff0
PierrickI3 <PierrickI3@users.noreply.github.com>
2014-10-30 10:14:34 +0100

    Delete fixlineendings.rb

--- templates/fixlineendings.rb
+++
@@ -1,6 +0,0 @@
-target="C:\\sql2014install.ini"
-target2="C:\\sql2014install2.ini"
-file_contents = File.read(target)
-new_contents = file_contents.gsub(/\r(?!\n)/, "\r\n")
-File.open(target, "w") {|file| file.puts new_contents }
-File.open(target2, "w") {|file| file.puts new_contents }
